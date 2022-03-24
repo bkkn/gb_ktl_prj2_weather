fe15a2939843c3607749eaeae3697248d258605c
bakaikin <bakaikin@yandex.ru>
2022-03-24 18:34:00 +0300

    Movies api key anf Loader added

--- movies/src/main/java/me/bkkn/gb_ktl_prj2_movies/data/FilmLoader.kt
+++ movies/src/main/java/me/bkkn/gb_ktl_prj2_movies/data/FilmLoader.kt
@@ -1,11 +1,10 @@
-package me.bkkn.gb_ktl_prj2_weather.data
+package me.bkkn.gb_ktl_prj2_movies.data
 
 import android.os.Build
 import android.os.Handler
 import android.util.Log
 import androidx.annotation.RequiresApi
 import com.google.gson.Gson
-import me.bkkn.gb_ktl_prj2_weather.BuildConfig
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.MalformedURLException
@@ -13,37 +12,42 @@ import java.net.URL
 import java.util.stream.Collectors
 import javax.net.ssl.HttpsURLConnection
 
-private const val WEATHER_API_KEY = "7838458d-c23e-4443-8d5a-2d90844d73af" // test tariff
-//2fefea651f90ea8fe7cba508a8c64a10
-class WeatherLoader(private val listener: WeatherLoaderListener, private val lat: Double, private val lon: Double) {
-    interface WeatherLoaderListener {
-        fun onLoaded(weatherDTO: WeatherDTO)
+private const val FILMS_API_KEY = "2fefea651f90ea8fe7cba508a8c64a10"
+
+
+class FilmLoader(
+    private val listener: FilmLoaderListener,
+    private val lat: Double,
+    private val lon: Double
+) {
+    interface FilmLoaderListener {
+        fun onLoaded(filmDTO: FilmDTO)
         fun onFailed(throwable: Throwable)
     }
 
     @RequiresApi(Build.VERSION_CODES.N)
-    fun loadWeather() {
+    fun loadFilm() {
         try {
             val uri =
-                URL("https://api.weather.yandex.ru/v2/informers?lat=${lat}&lon=${lon}")
+                URL("https://api.themoviedb.org/3/movie/550?api_key=${FILMS_API_KEY}")
             val handler = Handler()
             Thread(Runnable {
                 lateinit var urlConnection: HttpsURLConnection
                 try {
                     urlConnection = uri.openConnection() as HttpsURLConnection
                     urlConnection.requestMethod = "GET"
-                    urlConnection.addRequestProperty(
-                        "X-Yandex-API-Key",
-                        WEATHER_API_KEY
-                    )
+//                    urlConnection.addRequestProperty(
+//                        "X-Yandex-API-Key",
+//                        WEATHER_API_KEY
+//                    )
                     urlConnection.readTimeout = 10000
                     val bufferedReader =
                         BufferedReader(InputStreamReader(urlConnection.inputStream))
 
                     // преобразование ответа от сервера (JSON) в модель данных (WeatherDTO)
-                    val weatherDTO: WeatherDTO =
-                        Gson().fromJson(getLines(bufferedReader), WeatherDTO::class.java)
-                    handler.post { listener.onLoaded(weatherDTO) }
+                    val filmDTO: FilmDTO =
+                        Gson().fromJson(getLines(bufferedReader), FilmDTO::class.java)
+                    handler.post { listener.onLoaded(FilmDTO) }
                 } catch (e: Exception) {
                     Log.e("", "Fail connection", e)
                     e.printStackTrace()
